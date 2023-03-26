2db4d6aeb25559cee421e36d8b1751227623d9e9
inamnurul <aam.inam7310@gmail.com>
2023-03-26 16:46:34 +0700

    add: backspace logic

--- KeypadwithNokia2000Logic/KeypadwithNokia2000Logic.ino
+++ KeypadwithNokia2000Logic/KeypadwithNokia2000Logic.ino
@@ -1,8 +1,11 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 // Define the keypad 
-#define DELAY 100
-#define WAITING 1000
+#define DELAY 75
+#define WAITING 750
 const int ROWS = 4;
 const int COLS = 4;
+bool CAPSLOCK = true;
 String keys[ROWS][COLS] = {
   {" ", "ABC", "DEF", "BACKSPACE"},
   {"GHI", "JKL", "MNO", "CAPSLOCK"},
@@ -13,17 +16,24 @@ unsigned long onclickTime = 0;
 unsigned long onpressTime = 0;
 int numPresses = 0;
 String currentKey = "";
-
+String currentchar ="";
+String text="";
 
 // Define row and column
-int rowPins[ROWS] = {23, 22, 21, 19};
-int colPins[COLS] = {18, 5, 17, 16};
+int rowPins[ROWS] = {13, 12, 14, 27};
+int colPins[COLS] = {26, 25, 33, 32};
+
+Adafruit_SSD1306 display(128, 32, &Wire, -1);
 
 void setup() {
   Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.display();
 }
 
 void loop() {
+  
   for (int col = 0; col < COLS; col++) {
     
     pinMode(colPins[col], OUTPUT);
@@ -34,20 +44,38 @@ void loop() {
       pinMode(rowPins[row], INPUT_PULLUP);
       if((millis()-onclickTime)>DELAY){
         if (digitalRead(rowPins[row]) == LOW) {
-          if((millis()-onclickTime)<WAITING){
+          currentKey=keys[row][col];
+          if(row==0 && col==3){
+            text.remove(text.length()-1);
+          }
+          else if (row==1 && col==3){
+            CAPSLOCK= !CAPSLOCK;
+            Serial.print(CAPSLOCK);
+          }
+          else{
+            if((millis()-onpressTime)<WAITING){
             numPresses++;
             // Serial.print(numPresses);
-          }
-          else {
+            }
+            else {
             numPresses=0;
             // Serial.print(numPresses);
+            }
+            currentchar=currentKey.charAt(numPresses % currentKey.length());
+            // Serial.print(currentKey.charAt(numPresses % currentKey.length()));
+            onpressTime=millis();
+            onclickTime = millis();
           }
-          currentKey=keys[row][col];
-          Serial.print(currentKey.charAt(numPresses % currentKey.length()));
-          onclickTime = millis();
         }
       }
     }
+    if((millis()-onpressTime)>WAITING && currentchar != ""){
+      text+=currentchar;
+      currentchar="";
+      Serial.print(text);
+      Serial.print("\n");
+
+          }
     
     for (int row = 0; row < ROWS; row++) {
       pinMode(rowPins[row], OUTPUT);
@@ -56,4 +84,10 @@ void loop() {
     // Reset the current column to INPUT 
     pinMode(colPins[col], INPUT);
   }
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(text);
+  display.display();
 }
